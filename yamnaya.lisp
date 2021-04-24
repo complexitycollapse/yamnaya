632070bf8f0729b0a91bdc37955d5c12291d031f
complexitycollapse <complexitycollapse@protonmail.com>
2021-04-24 11:39:10 +0100

    Outputting comments

--- yamnaya.lisp
+++ yamnaya.lisp
@@ -4,7 +4,6 @@
 
 (defvar *current-stream*)
 (defvar *context*)
-(defvar *start-new-line* T)
 
 (defclass context ()
   ((indent :initarg :indent)
@@ -19,7 +18,8 @@
 (defun f-with-yaml-stream (stream fn)
   (let ((*current-stream* stream)
 	(*context* (make-context)))
-    (funcall fn)))
+    (funcall fn)
+    (fresh-line *current-stream*)))
 
 (defun message-separator ()
   (yaml-format "---"))
@@ -34,9 +34,9 @@
   (make-instance 'context :indent (1+ (slot-value context 'indent)) :parent context))
 
 (defun yaml-format (format-directive &rest format-args)
+  (fresh-line *current-stream*)
   (dotimes (n (* 2 (slot-value *context* 'indent))) (write-char #\space *current-stream*))
-  (apply #'format *current-stream* format-directive format-args)
-  (if *start-new-line* (fresh-line *current-stream*)))
+  (apply #'format *current-stream* format-directive format-args))
 
 (defun encode-object-member (key value)
   (yaml-format "~A: ~A" (encode-scalar key) (encode-scalar value)))
@@ -63,17 +63,30 @@
   (let ((*context* (indent)))
     (funcall fn)))
 
+(defun encode-comment (comment)
+  (yaml-format "# ~A" comment))
+
+(defun append-comment (comment)
+  (format *current-stream* " # ~A" comment))
+
 (defun foo ()
   (with-output-to-string (s)
-    (with-yaml-stream s (message-separator)
+    (with-yaml-stream s
+      (encode-comment "Here's some header material")
+      (message-separator)
       (encode-list-member "foo")
       (encode-list-member "bar")
+      (append-comment "Comment on bar")
       (with-list-member
-	(encode-list-member "indented"))
-      (encode-list-member "baz")
+	(append-comment "List comment")
+	(encode-list-member "indented")
+	(encode-list-member "baz"))
       (with-list-member
 	(encode-object-member "key" "val")
+	(append-commenT "Comment after first key")
+	(encode-comment "A comment on key2")
 	(encode-object-member "key2" "val2")
 	(with-object-member "complex"
 	  (encode-list-member "nested")
-	  (encode-list-member "list"))))))
+	  (encode-list-member "list")))
+      (message-terminator))))
